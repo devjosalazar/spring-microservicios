8c7011d414a04f307a9f3cbf11989752b9831206
josalazar <53492638+devjosalazar@users.noreply.github.com>
2024-04-23 17:45:27 -0500

    create and configure server

---
+++ regsitry-server/src/test/java/com/josalazar/regsitryserver/RegsitryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josalazar.regsitryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegsitryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
